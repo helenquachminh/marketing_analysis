ccb9f688bd408095446ce7baa85cb380ff28d335
Helen Quach <95054899+helenquachminh@users.noreply.github.com>
2021-12-06 22:19:14 +0000

    Create count_first_touch.sql

---
+++ count_first_touch.sql
@@ -0,0 +1,21 @@
+WITH first_touch AS (
+    SELECT user_id,
+        MIN(timestamp) as first_touch_at
+    FROM page_visits
+    GROUP BY user_id),
+ft_attr AS (
+  SELECT ft.user_id,
+         ft.first_touch_at,
+         pv.utm_source,
+         pv.utm_campaign
+  FROM first_touch ft
+  JOIN page_visits pv
+    ON ft.user_id = pv.user_id
+    AND ft.first_touch_at = pv.timestamp
+)
+SELECT ft_attr.utm_source,
+       ft_attr.utm_campaign,
+       COUNT(*) as total
+FROM ft_attr
+GROUP BY 1, 2
+ORDER BY 3 DESC;
